d6b8c41f615155be2690b522e135ea01eb750ce5
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2020-11-15 17:27:11 +0800

    增加表單傳送動畫，測試中...

--- js/FormSubmit.js
+++ js/FormSubmit.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    var modal = document.querySelector(".submit_modal");
 
     function validateEmail(email){
         const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -20,12 +21,23 @@ $(document).ready(function(){
         return false;
     }
 
+    function fadeOUT(){
+        setTimeout(function(){
+            var circle = document.querySelector(".outterCircle");
+            var loading = document.querySelector(".loading");
+            circle.classList.replace("outterCircle", "fadeOUT");
+            var success = '上傳成功!'
+            loading.innerHTML= success;
+            loading.classList.add("popUP");
+            modal.classList.add("fadeOUT");
+          }, 3000);
+      }
+
     // var form = document.getElementById("theForm");
     //     console.log(form);
 
     $("#theForm").submit(function(event){
         event.preventDefault();//防止頁面跳轉
-
         // var formData = {
         //     'name': $("#name").val(),
         //     'phone': $("#phone").val(),
@@ -35,23 +47,33 @@ $(document).ready(function(){
         //     'detail': $("#detail").val()
         // };
 
-        $.ajax({
-            type: 'post',
-            url: './php/process.php',
-            data: new FormData(this),
-            contentType: false,
-            cache: false,
-            processData: false,
-            success: function(data){
-                console.log(data);
-                validate;
-                alert("表單送出成功，謝謝您的聯絡。")
-                $("#theForm")[0].reset();
-            },
-            error: function(){
-                alert("表單寄送失敗!")
-            }
-        });
+        if($.trim($("#name").val()) === "" || $.trim($("#email").val()) === "" || $.trim($("#phone").val()) === ""){
+            alert('請確實填寫表單')
+            return false;
+        }else{
+            modal.classList.add("fadeIN");
+            modal.style['display'] = 'block';
+            fadeOUT();
+
+            $.ajax({
+                type: 'post',
+                url: './php/process.php',
+                data: new FormData(this),
+                contentType: false,
+                cache: false,
+                processData: false,
+                success: function(data){
+                    console.log(data);
+                    validate;
+                    $("#theForm")[0].reset();
+                },
+                error: function(){
+                    alert("表單寄送失敗!")
+                }
+            });
+        }
+
+        
 
     });
 });
