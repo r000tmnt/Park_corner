f519b011fad701b5b27356242261701273d6356e
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2020-11-19 14:45:11 +0800

    將 change 事件移動到 js 檔案

--- js/FormSubmit.js
+++ js/FormSubmit.js
@@ -11,6 +11,50 @@ $(document).ready(function(){
     //關閉modal後回到初始的樣子
     var recover = '<div class="outterCircle"><div class="innerCircle"></div></div><h3 class="loading">上傳中...</h3>';
 
+    
+    function validateEmail(email){
+        const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
+
+    function validate(){//驗證信箱地址是否正確
+        const email = $("#email").val();
+        let valid = document.getElementById("valid");
+
+        if(validateEmail(email)){
+            valid.innerHTML = email + "是有效的地址";
+            valid.style['color'] = 'green';
+        }else{
+            valid.innerHTML = email + "為無效的地址";
+            valid.style['color'] = 'red';
+        }
+        return false;
+    }
+
+    $("#email").change(validate);
+
+    var allow_ext = ["pdf", "doc", "docx", "jpg", "png", "jpeg", "gif"];
+    function checkEXT(inputFiles){//檢查夾帶的檔案類型
+        if(inputFiles.type == "file"){
+            var sFileName = inputFiles.value;
+            if(sFileName.length > 0){
+                var blnValid = false;
+                for(var i=0; i < allow_ext.length; i++){
+                    var sCurExtendion = allow_ext[i];
+                    if(sFileName.substr(sFileName.length - sCurExtendion.length, sCurExtendion.length).toLowerCase() == sCurExtendion.toLowerCase()){
+                        blnValid = true;
+                        break;
+                    }
+                }
+                if(!blnValid){
+                    alert("抱歉" + sFileName + "不符合接受的檔案類型, 允許的檔案為: " + allow_ext.join(","));
+                }
+            }
+        }
+    };
+
+    $("#file").change(checkEXT);
+
     $("#theForm").submit(function(event){
         event.preventDefault();//防止頁面跳轉
  
