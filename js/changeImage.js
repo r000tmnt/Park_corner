edc50f85bb234c62534ffa38b8e7b92cb69a4baa
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2022-03-19 17:56:04 +0800

    [WIP] 修改部分文案和翻譯，預計增各類風格的價格參考圖(想做切換功能)

---
+++ js/changeImage.js
@@ -0,0 +1,5 @@
+window.onload = function(){
+    let monochrome = [] //單色灰階的參考圖
+    let fullColor = [] //全彩圖的參考圖片
+    let fullShading = [] //仿厚圖的參考圖片
+}
--- js/changeLanguage.js
+++ js/changeLanguage.js
@@ -45,8 +45,8 @@ window.onload = function(){
             service: ["互助合作", "時間掌控", "多樣變化"],
             serDiscr: ["縮短想像與現實的距離，尋求雙方滿意的成品。", "不開天窗是接案人的職責，定期與委託人確認進度。", "簡單留白，賽璐璐或是油彩厚塗? 風格是可以切換的。"],
             AboutMe: ["半路覺醒成為獸控的阿宅，以畫筆表達對作品的喜好。曾經瘋了五年的忍者龜，對風格的影響顯而易見。", "作品參考:", "聯絡方式:"],
-            WorkFlows:["委託流程", "1.請聲明委託用途", "請以私訊或電子信件聯絡。 基本可用於個人收藏。", "上傳插畫至個人部落格(FB、Twitter)請標註繪師名稱。", "尺寸預設為A4，300dpi。 若有其他尺寸需求請事先告知。", "繪師接受原創、二創和獸人或NSFW...等題材，但不接受血腥、過激的元素。 請提供參考圖或是內容描述(越詳盡越好)。", "2.付款相關", "作品最多可提出3次修改，超過視情況加收費用。", "委託金可以分兩次或一次付清。兩次分為草稿確認與完稿確認。 台灣國內請匯至指定銀行帳戶，海外委託請用Paypal付款。", "繪師有權將委託視為自己的作品之一，並發布於社群網站(如Pixix、Twitter)。 若不願公開請事先告知。", "一般公司商業委託，請另外來信詢價。"],
-            table:["參考", "未列出格式不代表無法接受，可與繪師討論", "線搞", "賽璐璐", "油彩厚塗", "NT 300", "NT 1000", "NT 1700", "追加人物 NT 500(一個角色)", "加背景 NT 500~900(依複雜程度而定)", "委託約於一周內完成，繪師會與你保持聯絡追蹤進度"],
+            WorkFlows:["委託流程", "1.請聲明委託用途", "請以電子信件或本站的表單聯絡。 基本可用於個人收藏。", "上傳插畫至個人部落格(FB、Twitter)請標註繪師名稱。", "尺寸預設為A4，300dpi。 若有其他尺寸需求請事先告知。", "繪師接受原創、二創和獸人或NSFW...等題材，但不接受血腥、過激的元素。 請提供參考圖或是內容描述(越詳盡越好)。", "2.付款相關", "作品最多可提出3次修改，超過視情況加收費用。", "委託金可以分兩次或一次付清。兩次分為草稿確認與完稿確認。 台灣國內請匯至指定銀行帳戶，海外委託請用Paypal付款。", "繪師有權將委託視為自己的作品之一，並發布於社群網站(如Pixix、Twitter)。 若不願公開請事先告知。", "一般公司商業委託，請另外來信詢價。"],
+            table:["參考", "未列出格式不代表無法接受，可與繪師討論", "單色灰階", "全彩", "仿厚塗", "NT 450", "NT 1200", "NT 1800", "追加人物 NT 300(一個角色)", "加背景 NT 300~500(依複雜程度而定)", "委託約於案件確認的7至10天完成，繪師會與你保持聯絡追蹤進度"],
             Form:["*姓名: ", "電話: ", "*Email: ", "角色設計", "個人委託", "大頭貼", "書籍封面", "其他", "參考附件", "*可多選", "*詳細說明："]
         },
         eng: {
@@ -56,9 +56,9 @@ window.onload = function(){
             shortCaption: ["Help you perfect the image of your own character", "Shorten the distance between imagination and reality", "The sence of narrative at a glance", "Togither, we can make things better"],
             service: ["Mutual interest", "Time management", "Variaty of style"],
             serDiscr: ["Seek the produt that will satisfy both side.", "Freelance is duty to not meet the deadline, contact regularly.", "Simplify, cel-shading or oil paint-ish? The style is your choice."],
-            AboutMe: ["A nerd and a furry, often express his thoughts with brushes. Used to be a fan of TMNT, the show has it's impact on the style.", "Works reference:", "Contact:"],
-            WorkFlows:["Commission workflow", "1.State the purpose of the commission", "Please send the request by direct message or email. Basically, the commission work can be use for personal collection.", "Please tag the artist when you post the illustration on social media(FB, TWITTER...etc).", "The default size is A4 with 300dpi. Please state the number in cm or pixel if it is not A4.", "The artist accepts original ideas, deriviative work and furry. Themes with NSFW can be discuss, but not include gore.", "2.Payment", "The commission can be altered up to 3 times(Position, pose...etc). Extra fee will be charged if you wish to do more changes, the price depends on the complexity.", "The commission can be paid in full or split in two. Two means when the rough is confirm and when the work is complete.", "The artist will post the work on the social media. Please notify the artist if you don't want it open to public.", "For comerial cases, please make inquires with e-mail."],
-            table:["References", "*Formats not listed here can be discuss", "Line art", "Cel-shading", "Full-shading", "10 USD", "35 USD", "60 USD", "17 USD for additional character. (per character)", "17 ~ 31 USD for additional background. (depends on complexity)", "The commission is set to be finished in one week, the artist will contact the client to check on the porgress."],
+            AboutMe: ["A nerd and a furry, often express his thoughts with brushes. Used to be a fan of TMNT, the show has it's impact on the style.", "SNS:", "Contact:"],
+            WorkFlows:["Commission workflow", "1.State the purpose of the commission", "Please send the request by e-mail or contact form. Basically, the commission work can be use for personal collection.", "Please tag the artist when you post the illustration on social media(Facebook, TWITTER...etc).", "The default size is A4 with 300dpi. Please state the number in cm or pixel if it is not A4.", "The artist accepts original ideas, deriviative work and furry. Themes with NSFW can be discuss, but not include gore.", "2.Payment", "The commission can be altered up to 3 times(Position, pose...etc). Extra fee will be charged if you wish to do more changes, the price depends on the complexity.", "The commission can be paid in full or split in two. Two means when the rough is confirm and when the work is complete.", "The artist will post the work on the social media. Please notify the artist if you don't want it open to public.", "For comerial cases, please make inquires with e-mail."],
+            table:["Reference price", "*Formats not listed here can be discuss", "Monochrome", "Full color", "Full-shading", "15 USD", "40 USD", "50 USD", "10 USD for additional character. (per character)", "10 ~ 18 USD for additional background. (depends on complexity)", "The commission is set to be finished in 7 to 10 days, the artist will contact the client to check on the porgress."],
             Form:["*Name: ", "TEL: ", "*Email: ", "Character design", "Personal commission", "Profile pic", "Cover art", "Others", "Attachment", "*Multi-select", "*Description："]
         }
     }
@@ -119,11 +119,11 @@ window.onload = function(){
             form.querySelector(".text-right").innerHTML = `<input type="button" class="btn btn-secondary" value="${(attr == 'zh')? '取消' : 'Cancel'}"><input type="submit" class="btn btn-primary submitBtn" name="submit" value="${(attr == 'eng')? 'Submit' : '送出'}" style="margin-left: 2%;">`;
 
             //Table_mobile
-            trM[0].innerHTML = '<td>'+ languages[attr].table[2] +'</td><td data-th="線搞"><p class="demo bg-cover" style="background-image: url(images/Commission_Demo-Lineart01.jpg);"></p></td><td>'+ languages[attr].table[5] +'</td>';
+            trM[0].innerHTML = '<td>'+ languages[attr].table[2] +'</td><td data-th="Monochrome"><img class="demo" src="images/Ride_on_the_top.jpg" alt="參考圖"></td><td>'+ languages[attr].table[5] +'</td>';
 
-            trM[1].innerHTML = '<td>'+ languages[attr].table[3] +'</td><td data-th="賽璐璐"><p class="demo bg-cover" style="background-image: url(images/Commission_Demo-celShading.jpg);"></p></td><td>'+ languages[attr].table[6] +'</td>';
+            trM[1].innerHTML = '<td>'+ languages[attr].table[3] +'</td><td data-th="Full color"><img class="demo" src="images/Ride_on_the_top.jpg" alt="參考圖"></td><td>'+ languages[attr].table[6] +'</td>';
 
-            trM[2].innerHTML = '<td>'+ languages[attr].table[4] +'</td><td data-th="油彩厚塗"><p class="demo bg-cover" style="background-image: url(images/Commission_Demo-FullShading.jpg);"></p></td><td>'+ languages[attr].table[7] +'</td>'
+            trM[2].innerHTML = '<td>'+ languages[attr].table[4] +'</td><td data-th="Full-Shading"><img class="demo" src="images/Fundoshi_no_hi_2021.jpg" alt="參考圖"></td><td>'+ languages[attr].table[7] +'</td>'
 
             trM[3].innerHTML = ' <td>Notes</td><td><p>'+ languages[attr].table[8] +'</p><p>'+ languages[attr].table[9] +'</p><p>'+ languages[attr].table[10] +'</p></td>';
 
