30792ee78e024e87548ab0b0e525bfaf51172c25
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2023-04-18 11:13:00 +0800

    [WIP] Carousel, change language

--- component/carousel.js
+++ component/carousel.js
@@ -1,48 +1,28 @@
+const { useI18n } = VueI18n
+
 const carousel = {
     template: `
     <section class="Carousel">
         <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
-                <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-                <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-                <li data-target="#carouselExampleCaptions" data-slide-to="3"></li>
-                
+                <li
+                v-for="i in carousel_images.length"
+                :key="i" 
+                data-target="#carouselExampleCaptions" 
+                :data-slide-to="i-1" 
+                :class="{ active: (i-1) === 0}"
+                ></li>    
             </ol>
             <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <div class="Previous_works bg-cover" style="background-image: url('../images/Garant_the_dragon_monk_character_sheet.jpg');"></div>
+                <div v-for="(img, index) in carousel_images" :key="index" class="carousel-item" :class="{ active: index === 0 }">
+                    <div class="Previous_works bg-cover" :style="{'background-image': 'url('+ img +')'}"></div>
                     <div class="carousel-caption d-none d-md-block">
-                        <h5>角色設計</h5>
-                        <p>幫助你完善自創的角色形象</p>
+                        <h5>{{ tm('types')[index] }}</h5>
+                        <p>{{ tm('types_caption')[index] }}</p>
                     </div>
                 </div>
-
-                <div class="carousel-item">
-                    <div class="Previous_works bg-cover" style="background-image: url('../images/Yourpage_banner.jpg');"></div>
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>個人委託</h5>
-                        <p>縮短您心中的想像與現實的距離</p>
-                    </div>
-                </div>
-
-                <div class="carousel-item">
-                    <div class="Previous_works bg-cover" style="background-image: url('../images/2020.jpg');"></div>
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>封面設計</h5>
-                        <p>一眼感受到畫面的敘事性</p>
-                    </div>
-                </div>
-
-                <div class="carousel-item">
-                    <div class="Previous_works bg-cover" style="background-image: url('../images/Grant_blacki_deepred_dragon.jpg');"></div>
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>合作出版</h5>
-                        <p>集合眾人的力量</p>
-                    </div>
-                </div>
-
             </div>
+
             <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="sr-only">Previous</span>
@@ -54,9 +34,20 @@ const carousel = {
         </div>
     </section>
     `,    
-    props: ['lang'],
-    setup(props) {
-        console.log(props)
+    setup() {
+        const { tm } = useI18n()
+
+        const carousel_images = [
+            '../images/Garant_the_dragon_monk_character_sheet.jpg',
+            '../images/Yourpage_banner.jpg',
+            '../images/2020.jpg',
+            '../images/Grant_blacki_deepred_dragon.jpg'
+        ]
+
+        return {
+            carousel_images,
+            tm
+        }
     }
 }
 
--- js/locale.js
+++ js/locale.js
@@ -5,21 +5,60 @@
             eng: "English",
             about: "關於我",
             contact: "聯絡我",
-            types: "角色設計 | 個人委託 | 封面繪製 |合作出版",
-            types_caption: "幫助你完善自創的角色形象 | 縮短您心中的想像與現實的距離 | 一眼感受到畫面的敘事性 | 集合眾人的力量",
-            service: "互助合作 | 時間掌控 | 多樣變化",
-            serviceDesc: "縮短想像與現實的距離，尋求雙方滿意的成品。 | 不開天窗是接案人的職責，定期與委託人確認進度。 | 簡單留白，賽璐璐或是油彩厚塗? 風格是可以切換的。",
+            types: [
+                "角色設計",
+                "個人委託",
+                "封面繪製",
+                "合作出版"
+            ],
+            types_caption: [
+                "幫助你完善自創的角色形象",
+                "縮短您心中的想像與現實的距離",
+                "一眼感受到畫面的敘事性",
+                "集合眾人的力量"
+            ],
+            service: [
+                "互助合作",
+                "時間掌控",
+                "多樣變化"
+            ],
+            serviceDesc: [
+                "縮短想像與現實的距離，尋求雙方滿意的成品。",
+                "不開天窗是接案人的職責，定期與委託人確認進度。",
+                "簡單留白，賽璐璐或是油彩厚塗? 風格是可以切換的。"
+            ],
             AboutMe: "半路覺醒成為獸控的阿宅，以畫筆表達對作品的喜好。曾經瘋了五年的忍者龜，對風格的影響顯而易見。",
             link: "作品參考:", 
             email: "聯絡方式:",
             workFlow: "委託流程",
-            workFlow_title: "1.請聲明委託用途 | 2.付款相關",
-            workFlow_desc_1: "請以私訊或電子信件聯絡。 基本可用於個人收藏。 | 上傳插畫至個人部落格(FB、Twiiter)請標註繪師名稱。 | 尺寸預設為A4，300dpi。 若有其他尺寸需求請事先告知。 | 繪師接受原創、二創和獸人或NSFW...等題材，但不接受血腥、過激的元素。 請提供參考圖或是內容描述(越詳盡越好)。",
-            workFlow_desc_2: "作品最多可提出3次修改，超過視情況加收費用。 | 委託金可以分兩次或一次付清。兩次分為草稿確認與完稿確認。 台灣國內請匯至指定銀行帳戶，海外委託請用Paypal付款。 | 繪師有權將委託視為自己的作品之一，並發布於社群網站(如Pixix、Twitter)。 若不願公開請事先告知。 | 一般公司商業委託，請另外來信詢價。",
+            workFlow_title: [
+                "1.請聲明委託用途",
+                "2.付款相關"
+            ],
+            workFlow_desc_1: [
+                "請以私訊或電子信件聯絡。 基本可用於個人收藏。",
+                "上傳插畫至個人部落格(FB、Twiiter)請標註繪師名稱。",
+                "尺寸預設為A4，300dpi。 若有其他尺寸需求請事先告知。",
+                "繪師接受原創、二創和獸人或NSFW...等題材，但不接受血腥、過激的元素。 請提供參考圖或是內容描述(越詳盡越好)。"
+            ],
+            workFlow_desc_2: [
+                "作品最多可提出3次修改，超過視情況加收費用。",
+                "委託金可以分兩次或一次付清。兩次分為草稿確認與完稿確認。 台灣國內請匯至指定銀行帳戶，海外委託請用Paypal付款。",
+                "繪師有權將委託視為自己的作品之一，並發布於社群網站(如Pixiv、Twitter)。 若不願公開請事先告知。",
+                "一般公司商業委託，請另外來信詢價。"
+            ],
             reference: "參考",
             reference_note: "未列出格式不代表無法接受，可與繪師討論",
-            reference_styles: "灰階 | 賽璐璐 | 油彩厚塗",
-            reference_prices: "NT 450 | NT 1500 | NT 2550",
+            reference_styles: [
+                "灰階",
+                "賽璐璐",
+                "油彩厚塗"
+            ],
+            reference_prices: [
+                "NT 450",
+                "NT 1500",
+                "NT 2550"
+            ],
             reference_ps: "追加人物 NT 500(一個角色)\n加背景 NT 500~900(依複雜程度而定)\n繪師會與你聯絡確開始時間立與追蹤進度，委託預設一個禮拜完成",
             form_field_name: "姓名: ",
             form_field_tel: "電話: ",
@@ -37,21 +76,60 @@
             eng: "English",
             about: "About me",
             contact: "Contact me",
-            types: "Character design | Personal commission | Cover art | Co-publishing",
-            types_caption: "Help you perfect the image of your own character | Shorten the distance between imagination and reality | The sence of narrative at a glance | Together, we can make things better",
-            service: "Mutual interest | Time management | Variaty of style",
-            serviecDesc: "Seek the produt that will satisfy both side. | Freelance is duty to not meet the deadline, contact regularly. | Simplify, cel-shading or oil paint-ish? The style is your choice.",
+            types: [
+                "Character design",
+                "Personal commission",
+                "Cover art",
+                "Co-publishing"
+            ],
+            types_caption: [
+                "Help you perfect the image of your own character",
+                "Shorten the distance between imagination and reality",
+                "The sence of narrative at a glance",
+                "Together, we can make things better"
+            ],
+            service: [
+                "Mutual interest",
+                "Time management",
+                "Variaty of style"
+            ],
+            serviecDesc: [
+                "Seek the produt that will satisfy both side.",
+                "Freelance is duty to not meet the deadline, contact regularly.",
+                "Simplify, cel-shading or oil paint-ish? The style is your choice."
+            ],
             AboutMe: "A nerd and a furry, often express his thoughts with brushes. Used to be a fan of TMNT, the show has it's impact on the style.",
             link: "Reference work:", 
             email: "Contact:",
             workFlow: "Commission workflow",
-            workFlow_title: "1.State the purpose of the commission | 2.Payment",
-            workFlow_desc_1: "Please send the request by direct message or email. Basically, the commission work can be use for personal collection.。 | Please tag the artist when you post the illustration on social media(FB, TWITTER...etc). | The default size is A4 with 300dpi. Please state the number in cm or pixel if it is not A4. | The artist accepts original ideas, deriviative work and furry. Themes with NSFW can be discuss, but not include gore. Please provide with references when ask for commission.",
-            workFlow_desc_2: "The commission can be altered up to 3 times(Position, pose...etc). Extra fee will be charged if you wish to do more changes, the price depends on the complexity. | The commission can be paid in full or split in two. Two means when the rough is confirm and when the work is complete. Accept paypal for oversea client. | The artist will post the work on the social media. Please notify the artist if you don't want it open to public. | For commercial cases, please make inquires with e-mail.",
+            workFlow_title: [
+                "1.State the purpose of the commission",
+                "2.Payment"
+            ],
+            workFlow_desc_1: [
+                "Please send the request by direct message or email. Basically, the commission work can be use for personal collection.。",
+                "Please tag the artist when you post the illustration on social media(FB, TWITTER...etc).",
+                "The default size is A4 with 300dpi. Please state the number in cm or pixel if it is not A4.",
+                "The artist accepts original ideas, deriviative work and furry. Themes with NSFW can be discuss, but not include gore. Please provide with references when ask for commission."
+            ],
+            workFlow_desc_2: [
+                "The commission can be altered up to 3 times(Position, pose...etc). Extra fee will be charged if you wish to do more changes, the price depends on the complexity.",
+                "The commission can be paid in full or split in two. Two means when the rough is confirm and when the work is complete. Accept paypal for oversea client.",
+                "The artist will post the work on the social media. Please notify the artist if you don't want it open to public.",
+                "For commercial cases, please make inquires with e-mail."
+            ],
             reference: "References",
             reference_note: "*Formats not listed here can be discuss",
-            reference_styles: "Grayscale | Cel-shading | Full-shading",
-            reference_prices: "15 USD | 50 USD | 85 USD",
+            reference_styles: [
+                "Grayscale",
+                "Cel-shading",
+                "Full-shading"
+            ],
+            reference_prices: [
+                "15 USD",
+                "50 USD",
+                "85 USD"
+            ],
             reference_ps: "17 USD for additional character. (per character)\n17 ~ 31 USD for additional background. (depends on complexity)\nThe artist will contact the client to discuss about the context and when to start or check on the porgress. The default time span for a commission to be finished is one week.",
             form_field_name: "Name: ",
             form_field_tel: "TEL: ",
